8ab643ea11df77229f07d7de694b3bcc9339d37d
Akerke <akerke.kass@gmail.com>
2018-07-28 11:19:48 +0100

    improved nested list comprehensions in get_top_verbs function, all functions now contain description comments

--- frequently_used_verbs.py
+++ frequently_used_verbs.py
@@ -22,7 +22,6 @@ def main():
         This program outputs most frequently used verbs in the verb itself and occurence duo
         for given project names in a directory
     """
-
     words = []
 
     for project in PROJECTS:
@@ -35,14 +34,32 @@ def main():
 
 
 def get_top_verbs_in_path(path, top_size=10):
+    """
+    This function return top 10 verbs in a given path to filenames
+    :param path: string path to files
+    :param top_size: max number of verbs to display
+    :return: list of most commons verbs
+    """
     trees = [t for t in get_trees(path) if t]
-    functions = [f for f in flatten([[node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)] for t in trees]) if not (f.startswith('__') and f.endswith('__'))]
+    flattened_list = flatten([[node.name.lower() for node in ast.walk(t) if isinstance(node, ast.FunctionDef)] for t in trees])
+    functions = [f for f in flattened_list if not is_special_function()]
     print('functions extracted')
     verbs = flatten([get_verbs_from_function_name(function_name) for function_name in functions])
     return collections.Counter(verbs).most_common(top_size)
 
 
+def is_special_function():
+    """
+    This function checks if a function is special or not
+    :return: True/False
+    """
+    return (f.startswith('__') and f.endswith('__'))
+
+
 def get_trees(path, with_filenames=False, with_file_content=False):
+    """
+    This function generates trees of all filenames in a given path
+    """
     filenames = find_python_files(path=path, limit=100)
     print('total %s files' % len(filenames))
     trees = []
@@ -54,6 +71,12 @@ def get_trees(path, with_filenames=False, with_file_content=False):
 
 
 def find_python_files(path, limit):
+    """
+    This function scans a directory to find a particular extension file
+    :param path: the directory to scan
+    :param limit: max number of files
+    :return: list: list of filenames(strings)
+    """
     filenames = []
     for dirname, _, files in os.walk(path, topdown=True):
         if len(filenames) >= limit:
@@ -64,13 +87,20 @@ def find_python_files(path, limit):
 
 
 def custom_file_filter(files, dirname, extension=".py"):
+    """
+    This function searches for particular files, by default .py files.
+    :param files: name of files
+    :param dirname: target directory name
+    :param extension: particular extension
+    :return: path to the files
+    """
     return [os.path.join(dirname, file)
                 for f in files if f.endswith(extension)]
 
 
 def generate_trees(filename, tree, with_filenames, with_file_content):
     """
-        This function returns generates tree of filenames
+    This function returns generates tree of filenames
     """
     trees = []
     with open(filename, 'r', encoding='utf-8') as attempt_handler:
@@ -92,21 +122,21 @@ def generate_trees(filename, tree, with_filenames, with_file_content):
 
 def flatten(lst):
     """
-        This function flattens a list of tuples for one level
+    This function flattens a list of tuples for one level
     """
     return sum([list(item) for item in lst], [])
 
 
 def get_verbs_from_function_name(function_name):
     """
-        This function gives only verbs given a function name
+    This function gives only verbs given a function name
     """
     return [word for word in function_name.split('_') if is_verb(word)]
 
 
 def is_verb(word):
     """
-        This function checks whether the word is a verb or not
+    This function checks whether the word is a verb or not
     """
     if not word:
         return False
